be93d82a17aa0ec2e03c593097d89b0cb69e94de
shashwath63 <shashwathbn10@gmail.com>
2026-01-20 12:39:03 +0530

    feat: Integrate Winston for structured logging, enable New Relic log forwarding, and update the start script accordingly.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 require('newrelic');
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',//to set the log level
+  format: winston.format.json(),//to format the log in json
+  defaultMeta: { service: 'user-service' },//to add default meta data
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),//to store error logs in error.log file
+    new winston.transports.File({ filename: 'combined.log' }),//to store all logs in combined.log file
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}//to add console transport in development environment
 const  express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Hello World!');
-    res.json({ message: 'Hello World!' });
+    logger.log('route hit');
+    if (Math.random() < 0.5) {
+		logger.error("there was an err");
+	}
+    res.json({ message: 'hi there' });
 });
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');

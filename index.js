5f3904e4ff5556dab0c40c55c422f3f3bb2295c4
shashwath63 <shashwathbn10@gmail.com>
2026-01-20 11:59:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import 'newrelic';
+const  express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('Hello World!');
+    res.json({ message: 'Hello World!' });
+});
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
